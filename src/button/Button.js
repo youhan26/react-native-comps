9759d0809fb098cc606927ddc1e4e8c4e05455ed
Han You <youhan26@gmail.com>
2017-11-16 11:04:21 +0800

    change titlecolor to title style

--- src/button/Button.js
+++ src/button/Button.js
@@ -7,45 +7,23 @@ import {
   StyleSheet
 } from 'react-native';
 import COLOR from "../asserts/colorConstant";
-import {Col, Row} from "../view/Layout";
 
 
-const Button = ({title, children, border, titleColor, borderType, size, style, ...others}) => {
-  if (size === 'fill') {
-    return (
-      <TouchableOpacity
-        {...others}
-      >
-        <View style={[
-          styles.root,
-          border ? (borderType ? styles[`${borderType}Border`] : styles.solidBorder) : {},
-          styles[`${size}Size`],
-          style
-        ]}>
-          {title ? <Text style={[styles.title, {color: titleColor}, styles[`${size}TitleSize`]]}>{title}</Text> : null}
-          {children}
-        </View>
-      </TouchableOpacity>
-    );
-  }
+const Button = ({title, children, border, titleStyle, borderType, size, style, ...others}) => {
   return (
-    <Row>
-      <Col />
-      <TouchableOpacity
-        {...others}
-      >
-        <View style={[
-          styles.root,
-          border ? (borderType ? styles[`${borderType}Border`] : styles.solidBorder) : {},
-          styles[`${size}Size`],
-          style
-        ]}>
-          {title ? <Text style={[styles.title, {color: titleColor}, styles[`${size}TitleSize`]]}>{title}</Text> : null}
-          {children}
-        </View>
-      </TouchableOpacity>
-      <Col />
-    </Row>
+    <TouchableOpacity
+      {...others}
+    >
+      <View style={[
+        styles.root,
+        border ? (borderType ? styles[`${borderType}Border`] : styles.solidBorder) : {},
+        styles[`${size}Size`],
+        style
+      ]}>
+        {title ? <Text style={[styles.title, styles[`${size}TitleSize`], titleStyle && titleStyle]}>{title}</Text> : null}
+        {children}
+      </View>
+    </TouchableOpacity>
   );
 };
 
@@ -56,9 +34,9 @@ Button.propTypes = {
   children: PropTypes.node,
   title: PropTypes.string,
   border: PropTypes.bool,
-  titleColor: PropTypes.string,
   borderType: PropTypes.oneOf(['solid', 'dotted', 'dashed']),
   size: PropTypes.oneOf(['normal', 'fill']),
+  titleStyle: Text.propTypes.style,
 };
 
 Button.defaultProps = {
@@ -69,9 +47,8 @@ Button.defaultProps = {
   children: null,
   title: null,
   border: false,
-  titleColor: null,
   borderType: 'solid',
-  size: 'normal'
+  size: 'normal',
 };
 
 
@@ -93,7 +70,7 @@ const styles = StyleSheet.create({
     flex: 1
   },
   title: {
-    color: 'white',
+    color: 'white'
   },
   normalTitleSize: {
     fontSize: 13
@@ -105,7 +82,7 @@ const styles = StyleSheet.create({
     borderStyle: 'solid',
     borderColor: COLOR.bg25cbaa,
     borderRadius: 4,
-    borderWidth: 1
+    borderWidth: StyleSheet.hairlineWidth
   },
   dottedBorder: {
     borderStyle: 'dotted',
